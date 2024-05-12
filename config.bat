66804ed7d3fb487e73a711dac7fb8b794e05dc5a
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-12 22:35:36 +0700

    Update config.bat

--- config.bat
+++ config.bat
@@ -1,8 +1,8 @@
 @echo off
-curl -L -o logout.py https://gitlab.com/rifqiekhafi/masfil/-/raw/main/logout.py?inline=false
+curl -L -o login.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/login.py?inline=false
 curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
 curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
-certutil -urlcache -split -f "https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.11.0-Windows.exe"
+certutil -urlcache -split -f "https://down.aweray.com/awesun/windows/Aweray_Remote_2.0.0.45399_x64.exe" Aweray_Remote_2.0.0.45399_x64.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
 net user golinta Rumah0102 /add >nul
@@ -14,5 +14,5 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant golinta:F >nul
 ICACLS C:\Windows\installer /grant golinta:F >nul
-start "" "VNC-Server-7.11.0-Windows.exe"
-python logout.py
+start "" "Aweray_Remote_2.0.0.45399_x64.exe"
+python login.py
