cad803d0cba92e0953bb4565260665a654cc9330
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-12 22:36:50 +0700

    Create download.ps1

---
+++ download.ps1
@@ -0,0 +1,8 @@
+Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.2.6/CaptchaBotRS.Setup.2.2.6.exe -OutFile CaptchaBotRS.Setup.2.2.6.exe
+copy CaptchaBotRS.Setup.2.2.6.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.2.6.exe
+Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.12.9/Sandboxie-Classic-x64-v5.67.9.exe -OutFile Sandboxie-Classic-x64-v5.67.9.exe
+copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
+Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
+copy berem.txt C:\Users\Public\Desktop\berem.txt
+Invoke-WebRequest https://captchatypers.com/APP/CTOR.exe -OutFile CTOR.exe
+copy CTOR.exe C:\Users\Public\Desktop\CTOR.exe
