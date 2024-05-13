e2b9aafe447f7ebb3ac71128aaa6fe1a57560493
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-13 14:42:37 +0700

    Update config.bat

--- config.bat
+++ config.bat
@@ -5,14 +5,14 @@ curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?in
 certutil -urlcache -split -f "https://down.aweray.com/awesun/windows/Aweray_Remote_2.0.0.45399_x64.exe" Aweray_Remote_2.0.0.45399_x64.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
-net user runneradmin Rumah0102 /add >nul
-net localgroup administrators runneradmin /add >nul
-net user runneradmin /active:yes >nul
+net user khafidzu Rumah0102 /add >nul
+net localgroup administrators khafidzu /add >nul
+net user khafidzu /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant runneradmin:F >nul
-ICACLS C:\Windows\installer /grant runneradmin:F >nul
+ICACLS C:\Windows\Temp /grant khafidzu:F >nul
+ICACLS C:\Windows\installer /grant khafidzu:F >nul
 start "" "Aweray_Remote_2.0.0.45399_x64.exe"
 python login.py
