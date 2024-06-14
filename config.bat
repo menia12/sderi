fafea1875f1817cde902f7145a1f7bca95177d3e
menia12 <160221779+menia12@users.noreply.github.com>
2024-06-14 20:42:04 +0700

    Update config.bat

--- config.bat
+++ config.bat
@@ -2,7 +2,7 @@
 curl -L -o login.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/login.py?inline=false
 curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
 curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
-certutil -urlcache -split -f "https://down.aweray.com/awesun/windows/Aweray_Remote_2.0.0.45399_x64.exe" Aweray_Remote_2.0.0.45399_x64.exe
+certutil -urlcache -split -f "https://down.aweray.com/awesun/windows/Aweray_Remote_2.0.0.45399_x64.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
 net user khafidzu Rumah0102 /add >nul
