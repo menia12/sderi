d18b2db269da1f2315e33f9d6d58972cd5f29e5b
menia12 <160221779+menia12@users.noreply.github.com>
2024-08-08 13:50:28 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -4,5 +4,5 @@ Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/
 copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
 Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
 copy berem.txt C:\Users\Public\Desktop\berem.txt
-Invoke-WebRequest https://freevirtualkeyboard.com/FreeVKSetup.exe OutFile FreeVKSetup.exe
+Invoke-WebRequest https://freevirtualkeyboard.com/FreeVKSetup.exe -OutFile FreeVKSetup.exe
 copy FreeVKSetup.exe C:\Users\Public\Desktop\FreeVKSetup.exe
