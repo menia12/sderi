8749ae983f6c4f8cc8ed4a77a6c4a905486c382b
menia12 <160221779+menia12@users.noreply.github.com>
2025-02-08 10:28:26 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,7 +1,7 @@
-Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.3.3/CaptchaBotRS.Setup.2.3.3.exe -OutFile CaptchaBotRS.Setup.2.3.3.exe
-copy CaptchaBotRS.Setup.2.3.3.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.3.3.exe
-Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.12.9/Sandboxie-Classic-x64-v5.67.9.exe -OutFile Sandboxie-Classic-x64-v5.67.9.exe
-copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
+Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.3.4/CaptchaBotRS.Setup.2.3.4.exe -OutFile CaptchaBotRS.Setup.2.3.4.exe
+copy CaptchaBotRS.Setup.2.3.4.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.3.4.exe
+Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.15.4/Sandboxie-Plus-x64-v1.15.4.exe -OutFile Sandboxie-Plus-x64-v1.15.4.exe
+copy Sandboxie-Plus-x64-v1.15.4.exe C:\Users\Public\Desktop\Sandboxie-Plus-x64-v1.15.4.exe
 Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
 copy berem.txt C:\Users\Public\Desktop\berem.txt
 Invoke-WebRequest https://freevirtualkeyboard.com/FreeVKSetup.exe -OutFile FreeVKSetup.exe
