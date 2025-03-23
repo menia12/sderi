3bd3caa9902d526b37501b7d4e90f85f641513cd
menia12 <160221779+menia12@users.noreply.github.com>
2025-03-24 06:07:56 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.4.1/CaptchaBotRS.Setup.2.4.1.exe -OutFile CaptchaBotRS.Setup.2.4.1.exe
-copy CaptchaBotRS.Setup.2.4.1.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.4.1.exe
+Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.4.2/CaptchaBotRS.Setup.2.4.2.exe -OutFile CaptchaBotRS.Setup.2.4.2.exe
+copy CaptchaBotRS.Setup.2.4.2.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.4.2.exe
 Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.15.4/Sandboxie-Plus-x64-v1.15.4.exe -OutFile Sandboxie-Plus-x64-v1.15.4.exe
 copy Sandboxie-Plus-x64-v1.15.4.exe C:\Users\Public\Desktop\Sandboxie-Plus-x64-v1.15.4.exe
 Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
